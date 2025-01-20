addf998dbe5cdc53bc0617efba248a1e2ec892ab
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-01-20 15:43:37 +0530

    feat : add login and signup routes to routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./components/signup/signup.component').then(
+        (c) => c.SignupComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
