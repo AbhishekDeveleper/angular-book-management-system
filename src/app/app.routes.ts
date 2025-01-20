0c27c8e9082c52071f9d7bb05c68982e3e7160cd
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-01-20 16:46:41 +0530

    update : add new routes for books managment and navigaion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LibarianComponent } from './components/libarian/libarian.component';
+import { BookstoreComponent } from './components/bookstore/bookstore.component';
+import { OtheBookComponent } from './components/othe-book/othe-book.component';
+import { authGuard } from './auth.guard';
 export const routes: Routes = [
   {
     path: 'login',
@@ -13,6 +17,18 @@ export const routes: Routes = [
         (c) => c.SignupComponent
       ),
   },
+  {
+    path: 'addbook',
+    component: LibarianComponent,
+    canActivate: [authGuard],
+    title: 'libarian',
+  },
+  {
+    path: 'bookstore',
+    component: BookstoreComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'otherbook', component: OtheBookComponent },
   {
     path: '',
     redirectTo: 'login',
